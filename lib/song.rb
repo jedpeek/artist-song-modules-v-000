2842920e27f9d850622cf1b47f61296fe311dfcd
Jed Peek <jedpeek@yahoo.com>
2018-06-14 20:11:25 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ require 'pry'
 require_relative '../lib/concerns/memorable'
 require_relative '../lib/concerns/findable'
 class Song
-  extend Memorable
+  extend Memorable, Findable
   attr_accessor :name
   attr_reader :artist
 
