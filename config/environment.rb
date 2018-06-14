609db4aa49e25a70b777cd6417abe2fec4e8f6a0
Jed Peek <jedpeek@yahoo.com>
2018-06-14 19:46:07 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,11 @@ require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
 
 module Memorable
-  def self.reset_all
+  def reset_all
     self.all.clear
+  end
+
+  def self.count
+     @@artists.count
   end
 end
